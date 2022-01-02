33eaff789f09a182aa1534b14d4f141a789bbe1b
shaddy <shaddyx@gmail.com>
2022-01-02 12:30:12 +0200

    [-] removed redundant test

--- test/test_local/src/test_widget_utils.cpp
+++ test/test_local/src/test_widget_utils.cpp
@@ -35,21 +35,11 @@ namespace {
         TEST_ASSERT_EQUAL(1000, res);
    }
 
-   void test_calc_space_to_stretch_negative(){
-        Widget main("h:1000;horz:false,min_h:30");
-        Widget a1("h:10,hs:false,vs:true");
-        Widget a2("h:20,hs:false,vs:true");
-        main.add(a1);
-        main.add(a2);
-        auto res = widgetTools::calc_space_to_stretch(&main);
-        TEST_ASSERT_EQUAL(1000, res);
-   }
 }
 void test_widget_utils_main(){
     UNITY_BEGIN();
     RUN_TEST(test_get_stretchable_children);
     RUN_TEST(test_calc_space_to_stretch);
     RUN_TEST(test_calc_space_to_stretch_vert);
-    RUN_TEST(test_calc_space_to_stretch_negative);
     UNITY_END();
 }
