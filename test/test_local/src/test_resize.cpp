d7675d964e47adfe7841fcbe471e399a63d3f544
shaddy <shaddyx@gmail.com>
2022-01-07 13:19:24 +0200

    [-] latest fizes

--- test/test_local/src/test_resize.cpp
+++ test/test_local/src/test_resize.cpp
@@ -150,30 +150,6 @@ namespace {
         TEST_ASSERT_EQUAL(30, widgetB.get_w());
         TEST_ASSERT_EQUAL(100, widgetA.get_w());
     }
-
-      void test_resize_left_smaller_min_w_gigant(){
-          Widget widgetA;
-        Widget widgetB;
-        Widget widgetC;
-        widgetA.start()
-            -> add(& widgetB)
-            -> add(& widgetC)
-            -> set_horz(true)
-            -> set_w(100)
-            -> set_h(50);
-
-            widgetB.start()
-                -> set_hs(true)
-                -> set_min_w(10);
-            
-            widgetC.start()
-                -> set_hs(true)
-                -> set_min_w(70);
-        widgetA.poll();
-        TEST_ASSERT_EQUAL(70, widgetC.get_w());
-        TEST_ASSERT_EQUAL(30, widgetB.get_w());
-        TEST_ASSERT_EQUAL(100, widgetA.get_w());
-    }
 }
 void test_resize_main(){
     UNITY_BEGIN();
